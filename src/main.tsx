30ff8cc11737a26a985cf7daba19b098ee6df644
sanjaymusia-max <sanjaymusia@gmail.com>
2025-08-17 22:52:21 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import SafeParkSite from "./SafeParkSite";
+import SafeParkSite from "./SafeParkSite"; // make sure filename matches
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
