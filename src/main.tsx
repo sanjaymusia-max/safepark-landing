697f20e15d85a94f3af34941f0e470221856f560
sanjaymusia-max <sanjaymusia@gmail.com>
2025-08-17 22:34:14 +0530

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SafeParkSite from "./SafeParkSite";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SafeParkSite />
+  </React.StrictMode>
+);
